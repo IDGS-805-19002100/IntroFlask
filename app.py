03c2ca7c8f299d4c4d21877cadfc2c95680594c8
guillermopv1 <92887649+guillermopv1@users.noreply.github.com>
2026-01-27 23:29:17 -0600

    MensajesFlash

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
 
+import forms
 
 app=Flask(__name__)
+app.secret_key='clave secreta'
+
+csrf=CSRFProtect()
 
 
 @app.route("/")
@@ -10,9 +16,25 @@ def index():
     lista = ["Juan", "Pedro", "Mario"]
     return render_template("index.html", titulo=titulo, lista=lista)
 
-@app.route("/alumnos")
+@app.route("/alumnos",methods=['GET','POST'])
 def alumnos():
-    return render_template("alumnos.html")
+    mat=0
+    nom=''
+    apa=''
+    ama=''
+    email=''
+    alumnos_class=forms.UserForm(request.form)
+    if request.method=='POST' and alumnos_class.validate():
+        mat=alumnos_class.matricula.data
+        nom=alumnos_class.nombre.data
+        apa=alumnos_class.apaterno.data
+        ama=alumnos_class.amaterno.data
+        email=alumnos_class.correo.data
+        
+        mensaje='Bienvenido {}'.format(nom)
+        flash(mensaje)
+        
+    return render_template("alumnos.html",form=alumnos_class, mat=mat, nom=nom, apa=apa, ama=ama, email=email)
 
 @app.route("/usuarios")
 def usuarios():
@@ -38,15 +60,16 @@ def formulario():
         <input type="text" id="name" name="name" required>
     </form>
 '''
-@app.route("/operaBas")
+@app.route("/operaBas", methods=['GET', 'POST'])
 def opera1():
-    
-    if request.method=="POST":
-        n1=request.form.get("n1")
-        n2=request.form.get("n2")
-        res=int(n1)+int(n2)
+    res = 0
+    if request.method == "POST":
+        n1 = request.form.get("n1")
+        n2 = request.form.get("n2")
+        if n1 and n2:
+            res = int(n1) + int(n2)
         
-    return render_template("operaBas.html")
+    return render_template("operaBas.html", res=res)
 
 @app.route("/resultado",methods=['GET','POST'])
 def resultado():
@@ -56,4 +79,5 @@ def resultado():
     return "La suma de {} + {} = {}".format(n1,n2,res)
 
 if __name__=='__main__':
+    csrf.init_app(app)
     app.run(debug=True)
